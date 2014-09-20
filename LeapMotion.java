2f01bcbafc7822b359cfe6ac85ffed86d4d06382
heather huynh <amphy@uga.edu>
2014-09-20 01:14:56 -0400

    first commit

---
+++ LeapMotion.java
@@ -0,0 +1,23 @@
+/*
+* MPC Program
+* Sam R., Kevin E., Heather H.
+* 9/20/14
+*/
+
+import com.leapmotion.leap.*;
+
+public class LeapMotion {
+	
+	public static void main(String[] args) {
+	
+		// Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	
+	}
+	
+}
