5df42187a6a9ad1ca3010dba083bd2ff16814627
Sam Rakowski <devnull@iamdevnull.info>
2014-09-20 16:47:52 -0400

    edited indentation in file-space to tab

--- com/lithium/LithiumGui.java
+++ com/lithium/LithiumGui.java
@@ -7,107 +7,104 @@ import com.leapmotion.leap.*;
 import java.io.IOException;
 
 public class LithiumGui extends JFrame {
-  final static boolean shouldFill = true;
-  final static boolean shouldWeightX = true;
-  final static boolean RIGHT_TO_LEFT = false;
-  static JTextArea textArea;
-  
-  public static void addComponentsToPane(Container pane) {
-        if (RIGHT_TO_LEFT) {
-            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        }
- 
-        JButton button;
-    pane.setLayout(new GridBagLayout());
-    GridBagConstraints c = new GridBagConstraints();
-    if (shouldFill) {
-    //natural height, maximum width
-    c.fill = GridBagConstraints.HORIZONTAL;
-    }
- 
-    button = new JButton("Button 1");
-    if (shouldWeightX) {
-    c.weightx = 0.5;
-    }
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.gridx = 0;
-    c.gridy = 0;
-    pane.add(button, c);
- 
-    button = new JButton("Button 2");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0.5;
-    c.gridx = 1;
-    c.gridy = 0;
-    pane.add(button, c);
- 
-    button = new JButton("Button 3");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0.5;
-    c.gridx = 2;
-    c.gridy = 0;
-    pane.add(button, c);
- 
-    textArea = new JTextArea(5, 20);
-	textArea.setEditable(false);
-	JScrollPane scrollPane = new JScrollPane(textArea);
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.weightx = 0.0;
-    c.gridwidth = 3;
-    c.gridx = 0;
-    c.gridy = 1;
-    pane.add(textArea, c);
- 
-    button = new JButton("5");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.ipady = 0;       //reset to default
-    c.weighty = 1.0;   //request any extra vertical space
-    c.anchor = GridBagConstraints.PAGE_END; //bottom of space
-    c.insets = new Insets(10,0,0,0);  //top padding
-    c.gridx = 1;       //aligned with button 2
-    c.gridwidth = 2;   //2 columns wide
-    c.gridy = 2;       //third row
-    pane.add(button, c);
-    }
+	final static boolean shouldFill = true;
+	final static boolean shouldWeightX = true;
+	final static boolean RIGHT_TO_LEFT = false;
+	static JTextArea textArea;
+	
+	public static void addComponentsToPane(Container pane) {
+		if (RIGHT_TO_LEFT) {
+			pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		}
 
-  private static void createAndShowGUI() {
-	//Create and set up window
-	JFrame frame = new JFrame("Lithium");
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton button;
+		pane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		if (shouldFill) {
+			//natural height, maximum width
+			c.fill = GridBagConstraints.HORIZONTAL;
+		}
+ 
+		button = new JButton("Button 1");
+		if (shouldWeightX) {
+			c.weightx = 0.5;
+		}
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		pane.add(button, c);
+ 
+		button = new JButton("Button 2");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 1;
+		c.gridy = 0;
+		pane.add(button, c);
+ 
+		button = new JButton("Button 3");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.5;
+		c.gridx = 2;
+		c.gridy = 0;
+		pane.add(button, c);
+ 
+		textArea = new JTextArea(5, 20);
+		textArea.setEditable(false);
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 0.0;
+		c.gridwidth = 3;
+		c.gridx = 0;
+		c.gridy = 1;
+		pane.add(textArea, c);
+ 
+		button = new JButton("5");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.ipady = 0;			 //reset to default
+		c.weighty = 1.0;	 //request any extra vertical space
+		c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+		c.insets = new Insets(10,0,0,0);	//top padding
+		c.gridx = 1;			 //aligned with button 2
+		c.gridwidth = 2;	 //2 columns wide
+		c.gridy = 2;			 //third row
+		pane.add(button, c);
+		}
+
+	private static void createAndShowGUI() {
+		//Create and set up window
+		JFrame frame = new JFrame("Lithium");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
-	//Set up the content pane.
-	addComponentsToPane(frame.getContentPane());
+		//Set up the content pane.
+		addComponentsToPane(frame.getContentPane());
 	
-	//Display the window.
-	frame.pack();
-	frame.setVisible(true);
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
 	}
-  
-  public static void updateTextArea(String s) {
+	
+	public static void updateTextArea(String s) {
 		textArea.setText(s);
 	}
-  
-  public static void main(String[] args) {
-	javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	  public void run() {
-	    createAndShowGUI();
-	  }
-	 });
 	
-	Controller controller = new Controller();
-    GestureListener gl = new GestureListener();
-    
-    controller.addListener(gl);
-    
-    System.out.println("press enter to quit");
-    try {
-      System.in.read();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    
-    controller.removeListener(gl);
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
 	
-  }
-
+		Controller controller = new Controller();
+		GestureListener gl = new GestureListener();
+		
+		controller.addListener(gl);
+		
+		System.out.println("press enter to quit");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		controller.removeListener(gl);	
+	}
 }
