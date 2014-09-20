2bd750cda0e281a822b8ef49e30c987c6fc837a2
Sam Rakowski <devnull@iamdevnull.info>
2014-09-20 02:26:33 -0400

    added some skeleton files--functionality to be added later

---
+++ GestureListener.java
@@ -0,0 +1,19 @@
+package com.lithium;
+
+public class GestureListener {
+  private static SoundPlayer sp;
+  
+  public static void main(String[] args) {
+    // set up listener
+    // sp = new SoundPlayer();
+  }
+  
+  private void gestureRecieved(Gesture gesture) {
+    // we got a gesture.
+    // we want to play a sound.
+    // if (gesture == Gesture.LEFT) {
+    //   sp.playSound(SoundPlayer.LEFT);
+    // }
+    // (et c.)
+  }
+}
---
+++ SoundPlayer.java
@@ -0,0 +1,29 @@
+public class SoundPlayer {
+  // for swiping left
+  public final String LEFT = "sound.wav";
+  private Clip left_clip;
+  // for swiping right
+  public final String RIGHT = "sound2.wav";
+  private Clip right_clip;
+  // ...
+  
+  public SoundPlayer() {
+    // initialize Clip objects for wav files
+    // left_clip = new Clip(LEFT);
+    // (...)
+    // initialize output device
+    // (...)
+  }
+  
+  public void playSound(String sound) {
+    // if (sound == LEFT) {
+    //  playClip(leftClip);
+    // } else {
+    //   ...
+    // }
+  }
+  
+  private void playClip(Clip c) {
+    // os-specific method to play an audio clip
+  }
+}
