26d7345eea422cfdf202ddeba360a4e757434a34
Sam Rakowski <devnull@iamdevnull.info>
2014-09-20 04:55:02 -0400

    added new soundplayer, soundtester class to test the soundplayer

--- com/lithium/SoundPlayer.java
+++ com/lithium/SoundPlayer.java
@@ -1,31 +1,85 @@
 package com.lithium;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class SoundPlayer {
-  // for swiping left
-  public final String LEFT = "sound.wav";
-  private Clip left_clip;
-  // for swiping right
-  public final String RIGHT = "sound2.wav";
-  private Clip right_clip;
-  // ...
+  public static final String LEFT  = "res/sounds/clap.wav";
+  public static final String RIGHT = "res/sounds/cowbell.wav";
+  public static final String UP    = "res/sounds/hihat.wav";
+  public static final String DOWN  = "res/sounds/kick.wav";
+  public static final String TAP   = "res/sounds/snare.wav";
   
+  private AudioInputStream leftClip;
+  private AudioInputStream rightClip;
+  private AudioInputStream upClip;
+  private AudioInputStream downClip;
+  private AudioInputStream tapClip;
+
   public SoundPlayer() {
-    // initialize Clip objects for wav files
-    // left_clip = new Clip(LEFT);
-    // (...)
-    // initialize output device
-    // (...)
+    try {
+      leftClip  = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(LEFT)));
+      rightClip = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(RIGHT)));
+      upClip    = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(UP)));
+      downClip  = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(DOWN)));
+      tapClip   = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(TAP)));
+    } catch (UnsupportedAudioFileException e) {
+      e.printStackTrace();
+    } catch (FileNotFoundException e1) {
+      e1.printStackTrace();
+    } catch (IOException e2) {
+      e2.printStackTrace();
+    }
   }
   
   public void playSound(String sound) {
-    // if (sound == LEFT) {
-    //  playClip(leftClip);
-    // } else {
-    //   ...
-    // }
-  }
-  
-  private void playClip(Clip c) {
-    // os-specific method to play an audio clip
+    AudioInputStream theClip;
+    switch (sound) {
+      case LEFT:
+        theClip = leftClip;
+        break;
+      
+      case RIGHT:
+        theClip = rightClip;
+        break;
+      
+      case UP:
+        theClip = upClip;
+        break;
+      
+      case DOWN:
+        theClip = downClip;
+        break;
+      
+      case TAP:
+        theClip = tapClip;
+        break;
+        
+      default:
+        return;
+    }
+    
+    AudioFormat format = theClip.getFormat();
+    DataLine.Info info = new DataLine.Info(Clip.class, format);
+    try {
+      Clip audioClip = (Clip)AudioSystem.getLine(info);
+      audioClip.open(theClip);
+      audioClip.start();
+    } catch (LineUnavailableException e) {
+      e.printStackTrace();
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
   }
 }
---
+++ com/lithium/SoundTester.java
@@ -0,0 +1,32 @@
+package com.lithium;
+
+import java.io.IOException;
+
+public class SoundTester {
+  public static void main(String[] args) {
+    SoundPlayer sp = new SoundPlayer();
+    try {
+      System.out.println("left");
+      sp.playSound(SoundPlayer.LEFT);
+      Thread.sleep(1000);
+      
+      System.out.println("right");
+      sp.playSound(SoundPlayer.RIGHT);
+      Thread.sleep(1000);
+      
+      System.out.println("up");
+      sp.playSound(SoundPlayer.UP);
+      Thread.sleep(1000);
+      
+      System.out.println("down");
+      sp.playSound(SoundPlayer.DOWN);
+      Thread.sleep(1000);
+      
+      System.out.println("tap");
+      sp.playSound(SoundPlayer.TAP);
+      Thread.sleep(1000);
+    } catch (InterruptedException e1) {
+      e1.printStackTrace();
+    }
+  }
+}
